7bf1d5706fe5b2376b318d75e5d0c906e85bd244
Edward Dickson <ed.kickball@hotmail.com>
2019-04-24 17:43:39 +0100

    Update Analysis.r

--- Analysis.r
+++ Analysis.r
@@ -13,7 +13,7 @@ library("geosphere")
 #--------------------------------------------------------
 
 # i <- 0
-# googleAPIKey = "key=xxxx"
+# googleAPIKey = "key="
 # 
 # #https://stackoverflow.com/questions/13442461/populating-a-data-frame-in-r-in-a-loop
 # #https://stats.stackexchange.com/questions/5253/how-do-i-get-the-number-of-rows-of-a-data-frame-in-r
@@ -26,8 +26,10 @@ library("geosphere")
 #-------------------- LOAD DATASETS ---------------------
 #--------------------------------------------------------
 
+#https://stackoverflow.com/questions/13706188/importing-csv-file-into-r-numeric-values-read-as-characters
+
+nrWeatherIncident <- read.csv("C:\\Users\\Edward\\OneDrive\\Documents\\University\\4th Year\\Dissertation (BMAN31260)\\Data\\Network Rail Incidents\\National Rail - Weather Incident Export.csv", stringsAsFactors = FALSE, na.strings = c("", "#N/A", "NA", "N/A"))
 weatherStationDetails <- read.csv("C:\\Users\\Edward\\OneDrive\\Documents\\University\\4th Year\\Dissertation (BMAN31260)\\Data\\Weather Station\\Station Details.csv")
-nrWeatherIncident <- read.csv("C:\\Users\\Edward\\OneDrive\\Documents\\University\\4th Year\\Dissertation (BMAN31260)\\Data\\Network Rail Incidents\\National Rail - Weather Incident Export.csv")
 rain <- read.csv("C:\\Users\\Edward\\OneDrive\\Documents\\University\\4th Year\\Dissertation (BMAN31260)\\Data\\Rain\\midas_rainhrly_200601-200612.txt")
 soil <- read.csv("C:\\Users\\Edward\\OneDrive\\Documents\\University\\4th Year\\Dissertation (BMAN31260)\\Data\\Soil\\midas_soiltemp_200601-200612.txt")
 weather <- read.csv("C:\\Users\\Edward\\OneDrive\\Documents\\University\\4th Year\\Dissertation (BMAN31260)\\Data\\Weather (H)\\midas_wxhrly_200601-200612.txt")
@@ -35,13 +37,25 @@ wind <- read.csv("C:\\Users\\Edward\\OneDrive\\Documents\\University\\4th Year\\
 #postCodes <- read.csv("C:\\Users\\Edward\\OneDrive\\Documents\\University\\4th Year\\Dissertation (BMAN31260)\\Data\\Network Rail Incidents\\Postcodes.csv")
 
 #--------------------------------------------------------
-#-------------------- TRIM DATASETS ---------------------
+#---------------- TRIM DATASET COLUMNS ------------------
 #--------------------------------------------------------
 
+nrWeatherIncident <- subset(nrWeatherIncident, select = -c(Yr.Period))
 rain <- subset(rain, select = -c(ID, ID_TYPE, OB_HOUR_COUNT, VERSION_NUM, MET_DOMAIN_NAME, REC_ST_IND, PRCP_DUR, PRCP_AMT_Q, PRCP_DUR_Q, METO_STMP_TIME, MIDAS_STMP_ETIME, PRCP_AMT_J, X))
 soil <- subset(soil, select = -c(ID, ID_TYPE, REC_ST_IND, MET_DOMAIN_NAME, VERSION_NUM, METO_STMP_TIME, MIDAS_STMP_ETIME, Q5CM_SOIL_TEMP_Q, Q10CM_SOIL_TEMP_Q, Q20CM_SOIL_TEMP_Q, Q30CM_SOIL_TEMP_Q, Q50CM_SOIL_TEMP_Q, Q100CM_SOIL_TEMP_Q, Q5CM_SOIL_TEMP_J, Q10CM_SOIL_TEMP_J, Q20CM_SOIL_TEMP_J, Q30CM_SOIL_TEMP_J, Q50CM_SOIL_TEMP_J, Q100CM_SOIL_TEMP_J, X))
-weather <- subset(weather, select = -c(ID, ID_TYPE, MET_DOMAIN_NAME, VERSION_NUM, REC_ST_IND, WIND_SPEED_UNIT_ID, PRST_WX_ID, PAST_WX_ID_1, PAST_WX_ID_2, CLD_AMT_ID_1, CLOUD_TYPE_ID_1, CLD_BASE_HT_ID_1, CLD_AMT_ID_2, CLOUD_TYPE_ID_2, CLD_BASE_HT_ID_2, CLD_AMT_ID_3, CLOUD_TYPE_ID_3, CLD_BASE_HT_ID_3, CLD_AMT_ID_4, CLOUD_TYPE_ID_4, CLD_BASE_HT_ID_4, GROUND_STATE_ID, CAVOK_FLAG, CS_HR_SUN_DUR, WIND_DIRECTION_Q, WIND_SPEED_Q, PRST_WX_ID_Q, PAST_WX_ID_1_Q, PAST_WX_ID_2_Q, CLD_TTL_AMT_ID_Q, LOW_CLD_TYPE_ID_Q, MED_CLD_TYPE_ID_Q, HI_CLD_TYPE_ID_Q, CLD_BASE_AMT_ID_Q, CLD_BASE_HT_Q, VISIBILITY_Q, MSL_PRESSURE_Q, AIR_TEMPERATURE_Q, DEWPOINT_Q, WETB_TEMP_Q, GROUND_STATE_ID_Q, CLD_AMT_ID_1_Q, CLOUD_TYPE_ID_1_Q, CLD_BASE_HT_ID_1_Q, CLD_AMT_ID_2_Q, CLOUD_TYPE_ID_2_Q, CLD_BASE_HT_ID_2_Q, CLD_AMT_ID_3_Q, CLOUD_TYPE_ID_3_Q, CLD_BASE_HT_ID_3_Q, CLD_AMT_ID_4_Q, CLOUD_TYPE_ID_4_Q, CLD_BASE_HT_ID_4_Q, VERT_VSBY_Q, STN_PRES_Q, ALT_PRES_Q, Q10MNT_MXGST_SPD_Q, CS_HR_SUN_DUR_Q, WMO_HR_SUN_DUR_Q, METO_STMP_TIME, MIDAS_STMP_ETIME, WIND_DIRECTION_J, WIND_SPEED_J, PRST_WX_ID_J, PAST_WX_ID_1_J, PAST_WX_ID_2_J, CLD_AMT_ID_J, CLD_HT_J, VISIBILITY_J, MSL_PRESSURE_J, AIR_TEMPERATURE_J, DEWPOINT_J, WETB_TEMP_J, VERT_VSBY_J, STN_PRES_J, ALT_PRES_J, Q10MNT_MXGST_SPD_J, RLTV_HUM_J, SNOW_DEPTH_Q, DRV_HR_SUN_DUR, DRV_HR_SUN_DUR_Q, X))
+weather <- subset(weather, select = -c(ID, ID_TYPE, MET_DOMAIN_NAME, VERSION_NUM, REC_ST_IND, WIND_SPEED_UNIT_ID, PRST_WX_ID, PAST_WX_ID_1, PAST_WX_ID_2, CLD_AMT_ID_1, CLOUD_TYPE_ID_1, CLD_BASE_HT_ID_1, CLD_AMT_ID_2, CLOUD_TYPE_ID_2, CLD_BASE_HT_ID_2, CLD_AMT_ID_3, CLOUD_TYPE_ID_3, CLD_BASE_HT_ID_3, CLD_AMT_ID_4, CLOUD_TYPE_ID_4, CLD_BASE_HT_ID_4, VERT_VSBY, CAVOK_FLAG, CS_HR_SUN_DUR, WIND_DIRECTION_Q, WIND_SPEED_Q, PRST_WX_ID_Q, PAST_WX_ID_1_Q, PAST_WX_ID_2_Q, CLD_TTL_AMT_ID_Q, LOW_CLD_TYPE_ID_Q, MED_CLD_TYPE_ID_Q, HI_CLD_TYPE_ID_Q, CLD_BASE_AMT_ID_Q, CLD_BASE_HT_Q, VISIBILITY_Q, MSL_PRESSURE_Q, AIR_TEMPERATURE_Q, DEWPOINT_Q, WETB_TEMP_Q, GROUND_STATE_ID_Q, CLD_AMT_ID_1_Q, CLOUD_TYPE_ID_1_Q, CLD_BASE_HT_ID_1_Q, CLD_AMT_ID_2_Q, CLOUD_TYPE_ID_2_Q, CLD_BASE_HT_ID_2_Q, CLD_AMT_ID_3_Q, CLOUD_TYPE_ID_3_Q, CLD_BASE_HT_ID_3_Q, CLD_AMT_ID_4_Q, CLOUD_TYPE_ID_4_Q, CLD_BASE_HT_ID_4_Q, VERT_VSBY_Q, STN_PRES_Q, ALT_PRES_Q, Q10MNT_MXGST_SPD_Q, CS_HR_SUN_DUR_Q, WMO_HR_SUN_DUR_Q, METO_STMP_TIME, MIDAS_STMP_ETIME, WIND_DIRECTION_J, WIND_SPEED_J, PRST_WX_ID_J, PAST_WX_ID_1_J, PAST_WX_ID_2_J, CLD_AMT_ID_J, CLD_HT_J, VISIBILITY_J, MSL_PRESSURE_J, AIR_TEMPERATURE_J, DEWPOINT_J, WETB_TEMP_J, VERT_VSBY_J, STN_PRES_J, ALT_PRES_J, Q10MNT_MXGST_SPD_J, RLTV_HUM_J, SNOW_DEPTH_Q, DRV_HR_SUN_DUR, DRV_HR_SUN_DUR_Q, X))
 wind <- subset(wind, select = -c(ID_TYPE, ID, OB_HOUR_COUNT, MET_DOMAIN_NAME, VERSION_NUM, REC_ST_IND, MAX_GUST_CTIME, MEAN_WIND_DIR_Q, MEAN_WIND_SPEED_Q, MAX_GUST_DIR_Q, MAX_GUST_SPEED_Q, MAX_GUST_CTIME_Q, METO_STMP_TIME, MIDAS_STMP_ETIME, MEAN_WIND_DIR_J, MEAN_WIND_SPEED_J, MAX_GUST_DIR_J, MAX_GUST_SPEED_J))
+
+#--------------------------------------------------------
+#----------------- TRIM DATASET ROWS --------------------
+#--------------------------------------------------------
+
+#https://stackoverflow.com/questions/4862178/remove-rows-with-all-or-some-nas-missing-values-in-data-frame
+
+#remove rows without a postcode for the first location
+nrWeatherIncident <- nrWeatherIncident[complete.cases(nrWeatherIncident[,8]),]
+#remove rows with a location for the second location but not a postcode
+nrWeatherIncident <- nrWeatherIncident[!(complete.cases(nrWeatherIncident[,9]) & !complete.cases(nrWeatherIncident[,10])),]
 
 #--------------------------------------------------------
 #---------------- ADD LOCATION COLUMNS ------------------
@@ -55,6 +69,7 @@ rain[ , gpsColumns] <- NA
 soil[ , gpsColumns] <- NA
 weather[ , gpsColumns] <- NA
 wind[ , gpsColumns] <- NA
+remove(gpsColumns)
 
 #--------------------------------------------------------
 #-------------- POPULATE LOCATION COLUMNS ---------------
@@ -68,11 +83,15 @@ ind <- complete.cases(nrWeatherIncident[,17])
 #populate rows with a two Lat/Lons with great circle middle of both values
 nrWeatherIncident[ind, c("HIGH_PRCN_LON","HIGH_PRCN_LAT")] <- 
   with(nrWeatherIncident[ind,,drop=FALSE],
-       do.call(rbind, geosphere::gcIntermediate(cbind.data.frame(stanoxSplit1PostCodeLon, stanoxSplit1PostCodeLat),
-                                                cbind.data.frame(stanoxSplit2PostCodeLon, stanoxSplit2PostCodeLat), n = 1)))
+       geosphere::midPoint(cbind.data.frame(stanoxSplit1PostCodeLon, stanoxSplit1PostCodeLat),
+                                          cbind.data.frame(stanoxSplit2PostCodeLon, stanoxSplit2PostCodeLat)
+                                          )
+  )
 
 #populate rows with one Lat/Lon with those values
 nrWeatherIncident[!ind, c("HIGH_PRCN_LAT","HIGH_PRCN_LON")] <- nrWeatherIncident[!ind, c("stanoxSplit1PostCodeLat","stanoxSplit1PostCodeLon")]
+
+remove(ind)
 
 #https://stackoverflow.com/questions/21712384/updating-column-in-one-dataframe-with-value-from-another-dataframe-based-on-matc
 
@@ -105,26 +124,54 @@ wind <- wind[complete.cases(wind[,7:8]),]
 
 #https://stackoverflow.com/questions/18214395/add-empty-columns-to-a-dataframe-with-specified-names-from-a-vector
 
-stationColumn <- c("SRC_ID")
-nrWeatherIncident[ , stationColumn] <- NA
+stationColumns <- c("SRC_ID", "distance")
+nrWeatherIncident[ , stationColumns] <- NA
+remove(stationColumns)
 
 #--------------------------------------------------------
 #------------- POPULATE STATION ID COLUMN ---------------
 #--------------------------------------------------------
 
-#https://stackoverflow.com/questions/32618956/find-the-nearest-x-y-coordinate-using-r
+#https://stackoverflow.com/questions/55752064/r-finding-closest-coordinates-between-two-large-data-sets
 
-nrWeatherIncident$stationColumn<-weatherStationDetails[,1]
+nrWeatherIncident[ , c(20,21)] <- as.data.frame(RANN::nn2(weatherStationDetails[,c(4,5)],nrWeatherIncident[,c(18,19)],k=1))
+
+nrWeatherIncident[,20] <- weatherStationDetails[nrWeatherIncident[,21], 1]
+
+#https://stackoverflow.com/questions/55754660/finding-closest-points-between-multiple-datasets-using-ellipsoidal-vincenty/
+
+nrWeatherIncident[,c(20,21)] <- t(
+  apply(
+    apply(nrWeatherIncident[,c(18,19)], 1, function(mrow){distVincentyEllipsoid(mrow, weatherStationDetails[,c(4,5)])}), 
+    2, function(x){ c(SRC_ID=weatherStationDetails[which.min(x),1],distance=min(x))}
+  )
+)
+
+remove(weatherStationDetails)
 
 #https://www.rforexcelusers.com/vlookup-in-r/ - for merging stuff
+
+#--------------------------------------------------------
+#----------- REMOVE DISTANCES TOO FAR AWAY --------------
+#--------------------------------------------------------
+
+#https://stackoverflow.com/questions/8005154/conditionally-remove-dataframe-rows-with-r
+
+#nrWeatherIncident<-nrWeatherIncident[(nrWeatherIncident$distance<"0.04"),]
+
+#--------------------------------------------------------
+#-------------- POPULATE WEATHER COLUMNS ----------------
+#--------------------------------------------------------
+
+
 
 
 #--------------------------------------------------------
 #----------- CONVERT POSTCODES TO COORDINATES -----------
 #--------------------------------------------------------
 
-#https://allthingsr.blogspot.com/2012/01/geocode-your-data-using-r-json-and.html
-
+# #https://allthingsr.blogspot.com/2012/01/geocode-your-data-using-r-json-and.html
+# 
 # getGeoCode <- function(gcStr, googleAPIKey)
 # {
 #   gcStr <- gsub(' ','%20',gcStr) #Encode URL Parameters, by replacing spaces with %20
@@ -147,12 +194,12 @@ nrWeatherIncident$stationColumn<-weatherStationDetails[,1]
 #   names(gcodes) <- c("Lat", "Lng")
 #   return (gcodes)
 # }
-
-#https://stackoverflow.com/questions/13442461/populating-a-data-frame-in-r-in-a-loop
-#https://stackoverflow.com/questions/39556911/r-how-to-increment-the-incrementing-variable-within-a-for-loop
-#http://www.endmemo.com/program/R/rbind.php
-
-# for (postcode in postCodes$Postcodes){
+# 
+# #https://stackoverflow.com/questions/13442461/populating-a-data-frame-in-r-in-a-loop
+# #https://stackoverflow.com/questions/39556911/r-how-to-increment-the-incrementing-variable-within-a-for-loop
+# #http://www.endmemo.com/program/R/rbind.php
+# 
+# for (postcode in postCodes$Postcode){
 #   i <- i + 1
 #   coordinates <- getGeoCode(postcode, googleAPIKey)
 #   #print(coordinates)
@@ -162,7 +209,7 @@ nrWeatherIncident$stationColumn<-weatherStationDetails[,1]
 #   colnames(postcodeLocation) <- c("postcode", "HIGH_PRCN_LAT", "HIGH_PRCN_LON")
 #   stationLocations <- rbind(stationLocations, postcodeLocation)
 # }
-
-#https://stackoverflow.com/questions/15956183/how-to-save-a-data-frame-as-csv-to-a-user-selected-location-using-tcltk
-
-#write.csv(file="Location Reference Data.csv", x=stationLocations)
+# 
+# #https://stackoverflow.com/questions/15956183/how-to-save-a-data-frame-as-csv-to-a-user-selected-location-using-tcltk
+# 
+# write.csv(file="Location Reference Data.csv", x=stationLocations)
